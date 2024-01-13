048ebf4340760f918e0ac0e3eefee7236c732ba9
rosmis <rosmis123@gmail.com>
2024-01-13 01:04:59 +0100

    sizing correction

--- components/Ui/UiCarouselItem.vue
+++ components/Ui/UiCarouselItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="md:w-[150px] bg-contain bg-center bg-no-repeat border-b-0 border-slate-5 px-8 py-6  flex-shrink-0 w-[150px]"
+        class="md:w-[250px] bg-contain bg-center bg-no-repeat border-b-0 border-slate-5 px-8 py-6  flex-shrink-0 w-[150px]"
         :style="{
             backgroundImage: `url('${imageUrl}')`,
         }"
